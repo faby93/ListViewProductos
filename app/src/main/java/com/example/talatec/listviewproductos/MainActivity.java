a2304be2f084f1210d1cac8fce4487a8cd061edb
faby93 <fabiolaov93@gmail.com>
2018-04-24 11:17:10 -0500

    FloatingButton-Agregado de Productos

--- app/src/main/java/com/example/talatec/listviewproductos/MainActivity.java
+++ app/src/main/java/com/example/talatec/listviewproductos/MainActivity.java
@@ -8,14 +8,21 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     //variables a declarar
     private TextView txt; private
     ListView lista;
     //Datos con los que se llenara el ListView
-    private String productos[] = {"Computadora", "Mouse", "Dulces", "Hojas", "Lapices", "Lentes","Reloj", "Cuchara", "Celular", "Mesa", "Refrigerador", "Horno", "Audifonos"};
-    private String categoria[] = {"Electronica","Electronica","Dulceria","Papeleria","Papeleria","Moda","Perfumeria ", "Hogar", "Electronicos", "Hogar", "Electrodomesticos", "Electrodomesticos", "Electronica"};
+    private List<String> lProductos = new ArrayList<>();
+    private List<String> lcategoria = new ArrayList<>();
+
+    //private String productos[] = {"Computadora", "Mouse", "Dulces", "Hojas", "Lapices", "Lentes","Reloj", "Cuchara", "Celular", "Mesa", "Refrigerador", "Horno", "Audifonos"};
+    //private String categoria[] = {"Electronica","Electronica","Dulceria","Papeleria","Papeleria","Moda","Perfumeria ", "Hogar", "Electronicos", "Hogar", "Electrodomesticos", "Electrodomesticos", "Electronica"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         txt = (TextView) findViewById(R.id.textView);
         lista = (ListView) findViewById(R.id.lista);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, productos);
+
+        actualizarTabla();
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, productos);
         lista.setAdapter(adapter);
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -32,12 +41,35 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 txt.setText("Categoria elegido: " + categoria[position]);
             }
-        });
+        });*/
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        lProductos.add(data.getStringExtra("Nombre"));
+        lcategoria.add(data.getStringExtra("Categoria"));
+        //Toast.makeText(this, "AA : "+data.getStringExtra("Categoria"), Toast.LENGTH_LONG).show();
+        actualizarTabla();
+    }
+
+    private void actualizarTabla(){
+        String productos[] = new String[lProductos.size()];
+        lProductos.toArray(productos);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,productos);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                txt.setText("categoria elegido: " + lcategoria.get(position));
+            }
+        });
+
+    }
     public void llamaActividad(View x) {
         Intent i = new Intent(this,AgregarActividad.class);
-        startActivity(i);
+        startActivityForResult(i,123);
     }
 }
 
